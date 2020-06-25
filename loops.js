4a095712d6b217625d48ba85e39956563244a760
Kyle Nugent <kln5656@gmail.com>
2020-06-25 18:56:15 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -10,10 +10,10 @@ function forLoop(array) {
 }
 
 function whileLoop(n) {
-  let countdown = n;
+  //let countdown = n;
 
-    while (countdown > 0) {
-      console.log(--countdown)
+    while (n > 0) {
+      console.log(--n)
   }
     return "done";
 }
